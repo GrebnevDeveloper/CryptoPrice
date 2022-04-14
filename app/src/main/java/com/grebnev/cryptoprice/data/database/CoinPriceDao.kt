785fd4dc4425b34b1359cb0597ee18d7007704bc
grebnev <grebnev.developer@gmail.com>
2022-04-14 20:22:09 +0400

    Add local database

---
+++ app/src/main/java/com/grebnev/cryptoprice/data/database/CoinPriceDao.kt
@@ -0,0 +1,20 @@
+package com.grebnev.cryptoprice.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.grebnev.cryptoprice.data.pojo.price.CoinPrice
+
+@Dao
+interface CoinPriceDao {
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate")
+    fun getCoinPriceList(): LiveData<CoinPrice>
+
+    @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym LIMIT 1")
+    fun getCoinPriceFromSymbol(fSym: String): LiveData<CoinPrice>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCoinPriceList(coinPriceList: List<CoinPrice>)
+}
