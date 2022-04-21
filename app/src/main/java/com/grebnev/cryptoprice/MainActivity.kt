9b0fc85b4942a4952e54326239c2701b87d4a98c
grebnev <grebnev.developer@gmail.com>
2022-04-21 21:02:50 +0400

    Add coin view model

--- app/src/main/java/com/grebnev/cryptoprice/MainActivity.kt
+++ app/src/main/java/com/grebnev/cryptoprice/MainActivity.kt
@@ -1,33 +1,21 @@
 package com.grebnev.cryptoprice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.grebnev.cryptoprice.data.api.ApiFactory
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    private val compositeDisposable = CompositeDisposable()
 
+    private lateinit var coinViewModel: CoinViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val disposable = ApiFactory.apiService.getTopCoinsInfo()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_OF_LOADING_DATA", it.toString())
-            }, {
-                it.message?.let { message -> Log.d("TEST_OF_LOADING_DATA", message) }
-            })
-        compositeDisposable.add(disposable)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
+        coinViewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        coinViewModel.loadData()
+        coinViewModel.priceList.observe(this) {
+            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        }
     }
 }
