e9d00396774255b05208172f7c23fc2d92abaef3
grebnev <grebnev.developer@gmail.com>
2022-04-21 22:15:47 +0400

    Add detail info

--- app/src/main/java/com/grebnev/cryptoprice/MainActivity.kt
+++ app/src/main/java/com/grebnev/cryptoprice/MainActivity.kt
@@ -3,7 +3,6 @@ package com.grebnev.cryptoprice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         coinViewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        coinViewModel.loadData()
-        coinViewModel.priceList.observe(this) {
+//        coinViewModel.priceList.observe(this) {
+//            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+//        }
+        coinViewModel.getDetailInfo("BTC").observe(this) {
             Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
         }
     }
