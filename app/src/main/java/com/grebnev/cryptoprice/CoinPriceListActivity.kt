55ceb892d87f6126afb8d545718903fd263e3e85
grebnev <grebnev.developer@gmail.com>
2022-04-24 15:04:38 +0400

    Create adapter for RV coin price list

--- app/src/main/java/com/grebnev/cryptoprice/CoinPriceListActivity.kt
+++ app/src/main/java/com/grebnev/cryptoprice/CoinPriceListActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.grebnev.cryptoprice.data.pojo.price.CoinPrice
+import com.grebnev.cryptoprice.databinding.ActivityCoinPriceListBinding
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var coinViewModel: CoinViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_price_list)
+        val binding = ActivityCoinPriceListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val adapter = CoinPriceAdapter(this)
+        binding.rvCoinPriceList.adapter = adapter
         coinViewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-//        coinViewModel.priceList.observe(this) {
-//            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
-//        }
-        coinViewModel.getDetailInfo("BTC").observe(this) {
-            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        coinViewModel.priceList.observe(this) {
+            adapter.coinPriceList = it
+        }
+        adapter.onCoinClickListener = object : CoinPriceAdapter.OnCoinClickListener {
+            override fun onCoinClick(coin: CoinPrice) {
+                Log.d("COIN_CLICK_TEST", coin.fromSymbol)
+            }
+
         }
     }
 }
