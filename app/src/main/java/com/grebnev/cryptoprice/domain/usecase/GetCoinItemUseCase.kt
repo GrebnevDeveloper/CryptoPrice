a2357717997487cd24e173083a3bcc6f0a813664
grebnev <grebnev.developer@gmail.com>
2022-12-09 16:49:45 +0400

    Add domain layer

---
+++ app/src/main/java/com/grebnev/cryptoprice/domain/usecase/GetCoinItemUseCase.kt
@@ -0,0 +1,11 @@
+package com.grebnev.cryptoprice.domain.usecase
+
+import androidx.lifecycle.LiveData
+import com.grebnev.cryptoprice.domain.CoinRepository
+import com.grebnev.cryptoprice.domain.model.Coin
+
+class GetCoinItemUseCase(private val repository: CoinRepository) {
+    operator fun invoke(fromSymbol: String): LiveData<Coin> {
+        return repository.getCoinItem(fromSymbol)
+    }
+}
