f89a1e28713dc76ecff189c4f9d66209e12afb67
grebnev <grebnev.developer@gmail.com>
2022-12-22 14:46:22 +0400

    Inject repository for usecase

--- app/src/main/java/com/grebnev/cryptoprice/domain/usecase/GetCoinItemUseCase.kt
+++ app/src/main/java/com/grebnev/cryptoprice/domain/usecase/GetCoinItemUseCase.kt
@@ -3,8 +3,11 @@ package com.grebnev.cryptoprice.domain.usecase
 import androidx.lifecycle.LiveData
 import com.grebnev.cryptoprice.domain.CoinRepository
 import com.grebnev.cryptoprice.domain.entity.Coin
+import javax.inject.Inject
 
-class GetCoinItemUseCase(private val repository: CoinRepository) {
+class GetCoinItemUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
     operator fun invoke(fromSymbol: String): LiveData<Coin> {
         return repository.getCoinItem(fromSymbol)
     }
--- app/src/main/java/com/grebnev/cryptoprice/domain/usecase/GetCoinListUseCase.kt
+++ app/src/main/java/com/grebnev/cryptoprice/domain/usecase/GetCoinListUseCase.kt
@@ -3,8 +3,11 @@ package com.grebnev.cryptoprice.domain.usecase
 import androidx.lifecycle.LiveData
 import com.grebnev.cryptoprice.domain.CoinRepository
 import com.grebnev.cryptoprice.domain.entity.Coin
+import javax.inject.Inject
 
-class GetCoinListUseCase(private val repository: CoinRepository) {
+class GetCoinListUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
     operator fun invoke(): LiveData<List<Coin>> {
         return repository.getCoinList()
     }
