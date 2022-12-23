d8b08c11775548d26c3a8888fdd9860909620a23
grebnev <grebnev.developer@gmail.com>
2022-12-23 16:06:56 +0400

    Create worker factory

--- app/src/main/java/com/grebnev/cryptoprice/di/module/DataModule.kt
+++ app/src/main/java/com/grebnev/cryptoprice/di/module/DataModule.kt
@@ -3,6 +3,8 @@ package com.grebnev.cryptoprice.di.module
 import android.app.Application
 import com.grebnev.cryptoprice.data.database.AppDatabase
 import com.grebnev.cryptoprice.data.database.CoinDao
+import com.grebnev.cryptoprice.data.network.ApiFactory
+import com.grebnev.cryptoprice.data.network.ApiService
 import com.grebnev.cryptoprice.di.ApplicationScope
 import dagger.Module
 import dagger.Provides
@@ -12,9 +14,13 @@ class DataModule {
 
     @ApplicationScope
     @Provides
-    fun bindDatabaseSource(application: Application): CoinDao {
+    fun bindDatabase(application: Application): CoinDao {
         return AppDatabase.getInstance(application).coinDao()
     }
 
-
+    @ApplicationScope
+    @Provides
+    fun bindApiService(): ApiService {
+        return ApiFactory.apiService
+    }
 }
--- app/src/main/java/com/grebnev/cryptoprice/presentation/BaseApplication.kt
+++ app/src/main/java/com/grebnev/cryptoprice/presentation/BaseApplication.kt
@@ -1,12 +1,32 @@
 package com.grebnev.cryptoprice.presentation
 
 import android.app.Application
+import androidx.work.Configuration
+import com.grebnev.cryptoprice.data.database.AppDatabase
+import com.grebnev.cryptoprice.data.mapper.CoinMapper
+import com.grebnev.cryptoprice.data.network.ApiFactory
+import com.grebnev.cryptoprice.data.workers.RefreshDataWorkerFactory
 import com.grebnev.cryptoprice.di.DaggerApplicationComponent
+import javax.inject.Inject
 
-class BaseApplication : Application() {
+class BaseApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var refreshDataWorkerFactory: RefreshDataWorkerFactory
 
     val component by lazy {
         DaggerApplicationComponent.factory()
             .create(this)
+    }
+
+    override fun onCreate() {
+        component.inject(this)
+        super.onCreate()
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(refreshDataWorkerFactory)
+            .build()
     }
 }
