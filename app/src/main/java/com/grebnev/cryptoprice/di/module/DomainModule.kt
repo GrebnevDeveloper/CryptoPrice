bdb4be8a40da38a3de3b19744325678ba4b04ba6
grebnev <grebnev.developer@gmail.com>
2025-03-13 16:15:02 +0400

    separation coin on coin list and coin item

--- app/src/main/java/com/grebnev/cryptoprice/di/module/DomainModule.kt
+++ app/src/main/java/com/grebnev/cryptoprice/di/module/DomainModule.kt
@@ -1,6 +1,8 @@
 package com.grebnev.cryptoprice.di.module
 
+import com.grebnev.cryptoprice.data.repository.CoinListRepositoryImpl
 import com.grebnev.cryptoprice.data.repository.CoinRepositoryImpl
+import com.grebnev.cryptoprice.domain.repository.CoinListRepository
 import com.grebnev.cryptoprice.domain.repository.CoinRepository
 import dagger.Binds
 import dagger.Module
@@ -8,5 +10,8 @@ import dagger.Module
 @Module
 interface DomainModule {
     @Binds
-    fun bindRepository(impl: CoinRepositoryImpl): CoinRepository
+    fun bindCoinListRepository(impl: CoinListRepositoryImpl): CoinListRepository
+
+    @Binds
+    fun bindCoinRepository(impl: CoinRepositoryImpl): CoinRepository
 }
