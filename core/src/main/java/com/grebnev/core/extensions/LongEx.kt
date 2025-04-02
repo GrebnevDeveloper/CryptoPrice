67a31ed681a35a4df10ff5924e83aefee3fd0e45
grebnev <grebnev.developer@gmail.com>
2025-04-02 11:37:21 +0400

    refactor code

--- core/src/main/java/com/grebnev/core/extensions/LongEx.kt
+++ core/src/main/java/com/grebnev/core/extensions/LongEx.kt
@@ -5,12 +5,14 @@ import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
+private const val MILLISECONDS_IN_SECONDS = 1000L
+
 fun Long?.convertTimestampToTimeByPattern(
     pattern: String,
     timeZone: TimeZone = TimeZone.getDefault(),
 ): String {
     if (this == null) return ""
-    val date = Date(this * 1000)
+    val date = Date(this * MILLISECONDS_IN_SECONDS)
     val sdf = SimpleDateFormat(pattern, Locale.getDefault())
     sdf.timeZone = timeZone
     return sdf.format(date)
